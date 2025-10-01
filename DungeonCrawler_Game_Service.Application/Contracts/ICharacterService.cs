2615bcec791c3ddd9872e1b0149839a1c1e4454e
Timothée Millot <104968112+timotheemillot@users.noreply.github.com>
2025-10-01 13:19:03 +0200

    Create hero back + test

--- DungeonCrawler_Game_Service.Application/Contracts/ICharacterService.cs
+++ DungeonCrawler_Game_Service.Application/Contracts/ICharacterService.cs
@@ -9,7 +9,8 @@ public interface ICharacterService
     /// </summary>
     /// <param name="name">Nom du personnage</param>
     /// <param name="characterClass">Classe du personnage</param>
+    /// <param name="userId">Identifiant de l'utilisateur propriétaire du personnage</param>
     /// <returns></returns>
-    Task<Character> CreateCharacterAsync(string name, Classes characterClass);
+    Task<Character> CreateCharacterAsync(string name, Classes characterClass, string userId);
     
 }
